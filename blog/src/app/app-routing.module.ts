bf7a9d3987e2e8d1d12c548c0f234d1cca45c936
SylwekPabian <23137@student.pwsztar.edu.pl>
2019-04-05 16:53:38 +0200

    Add blog

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {ContactComponent} from "./components/contact/contact.component";
-import {BlogComponent} from "./components/blog/blog.component";
+import {BlogHomeComponent} from "./blog-home/blog-home.component";
 import {QuizComponent} from "./components/quiz/quiz.component";
+import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
 
 const routes: Routes = [
   {
@@ -16,11 +17,15 @@ const routes: Routes = [
   },
   {
     path: 'blog',
-    component: BlogComponent,
+    component: BlogHomeComponent,
   },
   {
     path: 'quiz',
     component: QuizComponent,
+  },
+  {
+    path: 'blog/detail/:id',
+    component: BlogItemDetailsComponent,
   }];
 
 
